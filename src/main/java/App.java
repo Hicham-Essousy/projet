133759acda6699c70546d0761b794fa21791cd2f
Hicham-Essousy <hicham.es-sousy@edu.uiz.ac.ma>
2021-12-21 16:46:44 +0100

    second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world from ds de QL");
+        System.out.println("Second Hello world from ds de QL");
     }
 }
